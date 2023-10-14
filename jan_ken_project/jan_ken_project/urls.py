e2d433abeb2362c0f862858fe04ad01d1253e0ee
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-14 19:18:45 +0900

    viewsファイルからfrontpageをimport

--- jan_ken_project/jan_ken_project/urls.py
+++ jan_ken_project/jan_ken_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-
+from jan_ken_app.views import frontpage
 urlpatterns = [
     path('admin/', admin.site.urls), #パスをadminに指定するとadmin.site.urlsに誘導される
     path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
