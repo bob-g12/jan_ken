8780ad3b988c07bd34d54088c5750f00fddbfe0a
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-16 10:24:06 +0900

    パスの設定

--- jan_ken_project/jan_ken_app/views.py
+++ jan_ken_project/jan_ken_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request,"jan_ken/lady.html")
+def drawpage(request):
+    return render(request,"jan_ken/draw.html")
+def winpage(request):
+    return render(request,"jan_ken/win.html")
+def losepage(request):
+    return render(request,"jan_ken/lose.html")
--- jan_ken_project/jan_ken_project/urls.py
+++ jan_ken_project/jan_ken_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from jan_ken_app.views import frontpage
+from jan_ken_app.views import drawpage , winpage , losepage
 urlpatterns = [
     path('admin/', admin.site.urls), #パスをadminに指定するとadmin.site.urlsに誘導される
     path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
+    path('draw.html',drawpage),
+    path('win.html',winpage),
+    path('lose.html',losepage),
 ]
