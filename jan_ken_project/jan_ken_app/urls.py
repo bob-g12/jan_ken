f112f174eab968c7c7ffe0a5da7c417d1e24d91b
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-17 21:46:10 +0900

    結果にランダム性を追加

--- jan_ken_project/jan_ken_app/urls.py
+++ jan_ken_project/jan_ken_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from jan_ken_app.views import frontpage
 from jan_ken_app.views import drawpage , winpage , losepage
+import random
+judgment = [drawpage,winpage,losepage]
+def random_def(low,high):
+    print (judgment[random.randint(low,high)])
 urlpatterns = [
     path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
-    path('draw.html',drawpage),
-    path('win.html',winpage),
-    path('lose.html',losepage),
+    path('draw.html',judgment[random.randint(0,2)]),
+    path('win.html',judgment[random.randint(0,2)]),
+    path('lose.html',judgment[random.randint(0,2)]),
     path('lady.html',frontpage),
 ]
