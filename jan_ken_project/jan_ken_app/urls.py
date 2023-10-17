6a920db33e7f47f6241e30afd19f6aa4cef5a0f5
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-17 18:22:27 +0900

    urls.pyの分割

---
+++ jan_ken_project/jan_ken_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from jan_ken_app.views import frontpage
+from jan_ken_app.views import drawpage , winpage , losepage
+urlpatterns = [
+    path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
+    path('draw.html',drawpage),
+    path('win.html',winpage),
+    path('lose.html',losepage),
+    path('lady.html',frontpage),
+]
--- jan_ken_project/jan_ken_project/urls.py
+++ jan_ken_project/jan_ken_project/urls.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from django.urls import path
-from jan_ken_app.views import frontpage
-from jan_ken_app.views import drawpage , winpage , losepage
+from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls), #パスをadminに指定するとadmin.site.urlsに誘導される
-    path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
-    path('draw.html',drawpage),
-    path('win.html',winpage),
-    path('lose.html',losepage),
-    path('lady.html',frontpage),
+    path("",include('jan_ken_app.urls')), #パスを指定しなかった場合frontpageに誘導される
 ]
