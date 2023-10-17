7b5c5fe67e3308d0ccc1dbb017eaf640157d00a2
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-18 01:19:28 +0900

    表示結果のランダム化

--- jan_ken_project/jan_ken_app/views.py
+++ jan_ken_project/jan_ken_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+import random
+judgment = ["jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html","jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html",
+            "jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html","jan_ken/super.html"]
+def random_def(low,high):
+    random_serect = None
+    random_serect = random.randint(low,high)
+    return random_serect
 def frontpage(request):
     return render(request,"jan_ken/lady.html")
 def drawpage(request):
-    return render(request,"jan_ken/draw.html")
+    return render(request,judgment[random_def(0,2)])
 def winpage(request):
-    return render(request,"jan_ken/win.html")
+    return render(request,judgment[random_def(0,2)])
 def losepage(request):
-    return render(request,"jan_ken/lose.html")
+    return render(request,judgment[random_def(0,2)])
