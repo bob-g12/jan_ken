58347923bf4f95ef5433cfc07e08a4cf87f59a73
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-18 01:18:58 +0900

    内容リセット

--- jan_ken_project/jan_ken_app/urls.py
+++ jan_ken_project/jan_ken_app/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from jan_ken_app.views import frontpage
 from jan_ken_app.views import drawpage , winpage , losepage
-import random
-judgment = [drawpage,winpage,losepage]
-def random_def(low,high):
-    print (judgment[random.randint(low,high)])
+
 urlpatterns = [
     path("",frontpage), #パスを指定しなかった場合frontpageに誘導される
-    path('draw.html',judgment[random.randint(0,2)]),
-    path('win.html',judgment[random.randint(0,2)]),
-    path('lose.html',judgment[random.randint(0,2)]),
+    path('draw.html',drawpage),
+    path('win.html',winpage),
+    path('lose.html',losepage),
     path('lady.html',frontpage),
 ]
