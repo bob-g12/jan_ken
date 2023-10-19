9c30b56a9ef7c2f1e0fa740298d7633d5b427db4
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-19 21:43:09 +0900

    累計ポイントと残りポイントのメッセージ表記

--- jan_ken_project/jan_ken_app/views.py
+++ jan_ken_project/jan_ken_app/views.py
@@ -5,10 +5,12 @@ count = 0
 counted = 0
 judgment = ["jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html","jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html",
             "jan_ken/draw.html","jan_ken/win.html","jan_ken/lose.html","jan_ken/super.html"]
-def random_def(low,high):
+#ジャンケン結果をランダムにする関数
+def random_def(low,high): 
     random_serect = random.randint(low,high)
     return random_serect
 
+#ジャンケンの得点を計算する関数
 def point(random_point):
     if judgment[random_point] == "jan_ken/lose.html":
         count = -1
@@ -20,19 +22,31 @@ def point(random_point):
         count = 0
     return count
 
+#Topページの関数
 def frontpage(request):
     context = {
         "count": counted
     }
     return render(request,"jan_ken/lady.html",context)
+
+#ランダムな結果を表示させる関数
 def judgpage(request):
     global counted
     random_count = random_def(0,len(judgment)-1)
     
     counted += point(random_count)
-    
+    if counted < 10:
+        not_clear = "あと"
+        clear_count =str(10 - counted)
+        not_point = "点"
+    else:
+        not_clear = ""
+        clear_count = "CLEAR"
+        not_point = ""
     context = {
-        "count": counted
+        "count": counted,
+        "score" : clear_count,
+        "limit" : not_clear,
+        "point" : not_point
     }
-    print(counted)
     return render(request,judgment[random_count],context)
