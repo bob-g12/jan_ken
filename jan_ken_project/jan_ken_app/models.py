a5e4287cfe4dfd246a8a5d44ab97e49f9eaf3367
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-22 01:28:30 +0900

    modelsにクラスの作成

--- jan_ken_project/jan_ken_app/models.py
+++ jan_ken_project/jan_ken_app/models.py
@@ -1,17 +1,16 @@
+# モデル読込
 from django.db import models
-# ユーザー認証
-from django.contrib.auth.models import User
 
-# ユーザーアカウントのモデルクラス
-class Account(models.Model):
+# モデルクラスを作成
+class People(models.Model):
+	# 項目定義
+    Name     = models.CharField(max_length=100)           # 文字列 最大文字数100
+    Tell     = models.IntegerField(blank=True, null=True) # 整数
+    Mail     = models.EmailField(max_length=100)          # Email
+    Birthday = models.DateField()                         # 日付
+    Website  = models.URLField()                          # URL
+    FreeText = models.TextField()                         # フリーテキスト
 
-    # ユーザー認証のインスタンス(1vs1関係)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    # 追加フィールド
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    account_image = models.ImageField(upload_to="profile_pics",blank=True)
-
+    # テキスト表示
     def __str__(self):
-        return self.user.username
+    	return self.Name
