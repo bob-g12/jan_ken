30692565abd460d060f9496fc8e0856b06b0a422
bob-g12 <sasakiT1019s@outlook.jp>
2023-10-24 00:42:39 +0900

    モデルクラスを用いたフォーム画面作成

--- jan_ken_project/jan_ken_app/forms.py
+++ jan_ken_project/jan_ken_app/forms.py
@@ -1,10 +1,22 @@
 from django import forms
+#モデルクラスを呼出
+from .models import People
 
 #フォームクラス作成
-class Contact_Form(forms.Form):
-    Name = forms.CharField(label="名前")                  
-    Tell = forms.IntegerField(label="電話番号") #s整数のみ入力可能な
-    Mail = forms.EmailField(label="メールアドレス")
-    Birthday = forms.DateField(label="生年月日")
-    Website = forms.URLField(label="Webサイト")
-    FreeText = forms.CharField(widget=forms.Textarea,label="備考")
+class Contact_Form(forms.ModelForm):
+
+    class Meta():
+        #①モデルクラスを指定
+        model = People
+
+        #②表示するモデルクラスのフィールドを定義
+        fields = ('Name','Tell','Mail','Birthday','Website','FreeText')
+
+        #③表示ラベルを定義
+        labels = {'Name':"名前",
+                  'Tell':"電話番号",
+                  'Mail':"メール",
+                  'Birthday':"生年月日",
+                  'Website':"Webサイト",
+                  'FreeText':"備考",
+        }
