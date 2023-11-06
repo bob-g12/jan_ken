1833fdf8e8a046ffe0ed4e4a427198db160b1355
bob-g12 <sasakiT1019s@outlook.jp>
2023-11-06 10:37:52 +0900

    IDとパスワードのみでログイン

--- jan_ken_project/account_app/views.py
+++ jan_ken_project/account_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # テンプレートタグ
-from .forms import AccountForm #AddAccountForm # ユーザーアカウントフォーム
+from .forms import AccountForm ,AddAccountForm # ユーザーアカウントフォーム
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -57,20 +57,20 @@ class  AccountRegistration(TemplateView):
         self.params = {
         "AccountCreate":False,
         "account_form": AccountForm(),
-        #"add_account_form":AddAccountForm(),
+        "add_account_form":AddAccountForm(),
         }
 
     #Get処理
     def get(self,request):
         self.params["account_form"] = AccountForm()
-        #self.params["add_account_form"] = AddAccountForm()
+        self.params["add_account_form"] = AddAccountForm()
         self.params["AccountCreate"] = False
         return render(request,"register.html",context=self.params)
 
     #Post処理
     def post(self,request):
         self.params["account_form"] = AccountForm(data=request.POST)
-        #self.params["add_account_form"] = AddAccountForm(data=request.POST)
+        self.params["add_account_form"] = AddAccountForm(data=request.POST)
 
         #フォーム入力の有効検証
         if self.params["account_form"].is_valid() and self.params["add_account_form"].is_valid():
@@ -125,7 +125,7 @@ class  AccountRegistration(TemplateView):
         #self.params["add_account_form"] = AddAccountForm(data=request.POST)
 
         # フォーム入力の有効検証
-        if self.params["account_form"].is_valid() and self.params["add_account_form"].is_valid():
+        if self.params["account_form"].is_valid() :#and self.params["add_account_form"].is_valid()
             # アカウント情報をDB保存
             account = self.params["account_form"].save()
             # パスワードをハッシュ化
@@ -135,16 +135,16 @@ class  AccountRegistration(TemplateView):
 
             # 下記追加情報
             # 下記操作のため、コミットなし
-            add_account = self.params["add_account_form"].save(commit=False)
+            #add_account = self.params["add_account_form"].save(commit=False)
             # AccountForm & AddAccountForm 1vs1 紐付け
-            add_account.user = account
+            #add_account.user = account
 
             # 画像アップロード有無検証
             #if 'account_image' in request.FILES:
                 #add_account.account_image = request.FILES['account_image']
 
             # モデル保存
-            add_account.save()
+            #add_account.save()
 
             # アカウント作成情報更新
             self.params["AccountCreate"] = True
